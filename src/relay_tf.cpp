c7462409ede597fe0bac2d4dd7ad4d1d634e1f90
Sees <Vsevolod.Egorov@ronavi.ru>
2024-04-05 22:37:12 +0300

    delete try/catch

--- src/relay_tf.cpp
+++ src/relay_tf.cpp
@@ -27,18 +27,11 @@ void RelayTF::listen(double rate, ros::Duration time)
   geometry_msgs::TransformStamped* transformStamped = new geometry_msgs::TransformStamped;
   while (ros::ok())
   {
-    try
+    for (const auto& tf_name : cfg_->getTFList())
     {
-      for (const auto& tf_name : cfg_->getTFList())
-      {
-        *transformStamped = buffer_->lookupTransform(tf_name.from, tf_name.to, ros::Time::now() - time);
-        transformStamped->header.stamp = ros::Time::now();
-        broadcaster_->sendTransform(*transformStamped);
-      }
-    }
-    catch (tf2::TransformException ex)
-    {
-      ROS_ERROR("%s", ex.what());
+      *transformStamped = buffer_->lookupTransform(tf_name.from, tf_name.to, ros::Time::now() - time);
+      transformStamped->header.stamp = ros::Time::now();
+      broadcaster_->sendTransform(*transformStamped);
     }
     loop_rate.sleep();
   }
